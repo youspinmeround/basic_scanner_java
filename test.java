d35292366c2957cdcd495c37262451cc37a16405
Nikita <72617418+youspinmeround@users.noreply.github.com>
2020-11-09 00:22:59 +0600

    Yes
    Yes

---
+++ test.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class test {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String message = scanner.nextLine();
+        System.out.println(message);
+
+
+
+
+
+
+
+    }
+}
